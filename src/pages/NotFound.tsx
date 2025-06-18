6965b2bf14f1dddfffdfd7758782c4fc344836b6
Akin <aevans@sageoak.education>
2025-06-18 14:45:58 -0700

    Build 404 Not Found page. Begin building Admin dashboard

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{ width: '100vw', height: '10vh' }}
+      className='404-wrapper d-flex flex-column justify-content-center align-items-center'
+    >
+      <h1>404 Page Not Found</h1>
+      <a href='/'>Return Home</a>
+    </div>
+  );
+}
