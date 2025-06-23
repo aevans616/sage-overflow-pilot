895e143648d70b8e3f0bc6b6b5853486de122ed4
Akin <aevans@sageoak.education>
2025-06-23 13:23:03 -0700

    Add Searchbar to Article Page. Update Not Found Page width, add arrowRight asset

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import ArticleCard from '../components/ArticleCard';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const supabase = createClient(
@@ -70,6 +75,54 @@ export default function Articles() {
   return (
     <>
       <div className='content-wrapper w-75 d-flex flex-column justify-content-center text-center mb-3 '>
+        <Navbar
+          style={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '1rem',
+            padding: '1rem',
+            boxShadow: '-2px 2px 4px rgba(0, 0, 0, 0.15)',
+          }}
+          className='bg-primary'
+        >
+          <h2 className='mb-4 text-white'>Looking for help?</h2>
+          <Form
+            style={{
+              width: 'auto',
+              maxWidth: '665px',
+            }}
+          >
+            <Row
+              style={{
+                margin: '0',
+                padding: '0',
+              }}
+            >
+              <Col
+                style={{
+                  margin: '0',
+                  width: '50%',
+                  maxWidth: '400px',
+                }}
+              >
+                <Form.Control type='text' placeholder='Search for an article' />
+              </Col>
+              <Col xs='auto'>
+                <Button
+                  style={{
+                    backgroundColor: 'white',
+                  }}
+                  type='submit'
+                >
+                  Search
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Navbar>
         <div
           className='sort-by-wrapper'
           style={{
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <div
-      style={{ width: '75vw', height: '10vh' }}
-      className='404-wrapper d-flex flex-column justify-content-center align-items-center'
+      style={{ width: '75vw', height: '80vh' }}
+      className='404-wrapper d-flex flex-column justify-content-flex-start align-items-center'
     >
       <h1>404 Page Not Found</h1>
       <a href='/'>Return Home</a>
