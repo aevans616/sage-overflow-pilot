49824708ab19082e480b93fbdcb5dd5f88e70b2b
Akin <aevans@sageoak.education>
2025-06-24 13:38:57 -0700

    Add functionality to provide an article title, replace component div wrapper witha  Form component

--- src/pages/ArticleForm.tsx
+++ src/pages/ArticleForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useMemo, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import JoditEditor from 'jodit-react';
 import {
   getLastArticleId,
@@ -17,6 +17,7 @@ console.clear();
 
 export default function ArticleForm({ placeholder }) {
   const editor = useRef(null);
+  const [articleTitle, setArticleTitle] = useState('');
   const [content, setContent] = useState('');
   const [lastId, setLastId] = useState(1); // for storing the latest article id in Supabase
 
@@ -24,30 +25,30 @@ export default function ArticleForm({ placeholder }) {
   interface ArticleData {
     id: number;
     author_id: number;
+    last_editor_id: number;
+    title: string;
+    content: string | any;
     created_at: Date;
+    is_published: boolean;
     last_modified: Date;
     archive_date: Date | null;
-    last_editor_id: number;
     view_count: number;
-    content: string | any;
-    is_published: boolean;
   }
 
   const newArticle: ArticleData = {
-    id: lastId, // hardcoded for now
+    id: lastId,
     author_id: 101, // hardcoded until login functionality / user roster is built
+    last_editor_id: 101,
+    title: articleTitle,
+    content: content, // content row === content state var
     created_at: new Date(),
+    is_published: true,
     last_modified: new Date(),
     archive_date: null,
-    last_editor_id: 101,
     view_count: 1,
-    //^ For view_count get data from backend if article already exists, otherwise set to 1
-    content: content,
-    //^ content row === content state var
-    is_published: true,
   };
 
-  console.log('next article_id will be: ' + newArticle.id);
+  // console.log('next article_id will be: ' + newArticle.id);
 
   const config = useMemo(
     () => ({
@@ -62,69 +63,92 @@ export default function ArticleForm({ placeholder }) {
   }, []); // Empty dependency array, run only once
 
   return (
-    <div
-      className='form-wrapper'
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        gap: '1rem',
-        width: '100%',
-      }}
-    >
-      <JoditEditor
-        ref={editor}
-        value={content}
-        //^ value will be set to data from backend or "content" for new text
-        config={config}
-        tabIndex={1} // tabIndex of the editor
-        onBlur={(newContent) => setContent(newContent)} // Update content on blur for performance reasons
-        onChange={(newContent) => {
-          setContent(newContent);
-          // console.log(newContent);
-        }}
-      />
-      <div
-        className='buttons-wrapper'
+    <Form>
+      <Form.Group
+        className='mb-3'
+        controlId='formBasicEmail'
         style={{
           display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'flex-end',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
           alignItems: 'center',
-          gap: '1rem',
-          width: '90%',
+          gap: '0.25rem',
+          width: '100%',
         }}
       >
-        <Button
-          id='form-publish-btn'
-          type='button'
-          style={{
-            width: '8rem',
-            background: 'transparent',
-            border: '2px solid #83a18a',
+        <Form.Label style={{ width: '100%', textAlign: 'left' }}>
+          Article Title
+        </Form.Label>
+        {/* //TODO get title data, place into state, include in ArticleData interface */}
+        <Form.Control
+          type='text'
+          placeholder='Enter Title'
+          onChange={(event) => {
+            setArticleTitle(event.target.value);
+            console.log(articleTitle);
           }}
-          onClick={() => {
-            alert('save draft');
+        />
+
+        <Form.Label
+          style={{ width: '100%', marginTop: '2rem', textAlign: 'left' }}
+        >
+          Article Content
+        </Form.Label>
+        <JoditEditor
+          className='w-100'
+          ref={editor}
+          value={content}
+          //^ value will be set to data from backend or "content" for new text
+          config={config}
+          tabIndex={1} // tabIndex of the editor
+          onBlur={(newContent) => setContent(newContent)} // Update content on blur for performance reasons
+          onChange={(newContent) => {
+            setContent(newContent);
+            // console.log(newContent);
+          }}
+        />
+        <div
+          className='buttons-wrapper'
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            gap: '1rem',
+            width: '100%',
+            marginTop: '2rem',
           }}
         >
-          Save Draft
-        </Button>
-        <Button
-          id='form-draft-btn'
-          type='button'
-          style={{
-            width: '8rem',
-          }}
-          onClick={() => {
-            // push article data to supabase
-            publishArticle(supabase, newArticle);
-            console.log(content);
-          }}
-        >
-          Publish
-        </Button>
-      </div>
-    </div>
+          <Button
+            id='form-publish-btn'
+            type='button'
+            style={{
+              width: '8rem',
+              background: 'transparent',
+              border: '2px solid #83a18a',
+            }}
+            onClick={() => {
+              alert('save draft');
+            }}
+          >
+            Save Draft
+          </Button>
+          <Button
+            id='form-draft-btn'
+            type='button'
+            style={{
+              width: '8rem',
+            }}
+            onClick={() => {
+              // push article data to supabase
+              publishArticle(supabase, newArticle);
+              console.log(content);
+            }}
+          >
+            Publish
+          </Button>
+        </div>
+      </Form.Group>
+    </Form>
   );
 }
